0ac1dc703e3720f3f114adfbc5cdecac9e20b5af
swathi <swathimaddipatla11@gmail.com>
2024-11-02 07:32:22 +0530

    weather api

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+
+
+export default App
